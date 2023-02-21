f95a30ab32b4bed2d4992544423a10770f7eae1a
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2023-02-22 00:42:58 +0100

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -23,19 +23,23 @@ echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "SELECT MAX(winner_goals) FROM games;")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo
+echo "$($PSQL "SELECT COUNT(game_id) FROM games WHERE winner_goals>2 ;")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo
+echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON winner_id=team_id WHERE round='Final' AND year=2018;")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT name FROM teams t JOIN(SELECT DISTINCT winner_id FROM games WHERE round='Eighth-Final' AND year=2014)winners ON t.team_id = winners.winner_id UNION ALL SELECT name FROM teams t JOIN(SELECT DISTINCT opponent_id FROM games WHERE round='Eighth-Final' AND year=2014)opponents ON t.team_id = opponents.opponent_id ORDER BY name; ")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT name FROM teams t JOIN(SELECT DISTINCT winner_id FROM games)winners ON t.team_id = winners.winner_id ORDER BY name;")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT year,name FROM games LEFT JOIN teams ON games.winner_id = teams.team_id WHERE round='Final' ORDER BY year;")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%';")"
+
+
+
+
